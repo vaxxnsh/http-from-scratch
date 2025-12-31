73dd5a048fb70371001c6636b4c5485505189ed0
vaxxnsh <kakshit817@gmail.com>
2025-12-31 07:38:56 +0530

    task: http-from-scratch ch-1 l-5

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
 const FILE_PATH = "messages.txt"
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		part := ""
+
+		for {
+			data := make([]byte, 8)
+			_, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			if newLineIdx := bytes.IndexByte(data, '\n'); newLineIdx != -1 {
+				part += string(data[:newLineIdx])
+				data = data[newLineIdx+1:]
+				out <- part
+				part = ""
+			}
+			part += string(data)
+		}
+
+		if len(part) != 0 {
+			out <- part
+		}
+	}()
+
+	return out
+}
 
 func main() {
 	f, err := os.Open(FILE_PATH)
@@ -15,12 +48,9 @@ func main() {
 		log.Fatalf("error: couldn't open the file %s\n%s", FILE_PATH, err)
 	}
 
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-		fmt.Printf("read: %v\n", string(data[:n]))
+	lines := getLinesChannel(f)
+
+	for line := range lines {
+		fmt.Printf("read: %v\n", line)
 	}
 }
