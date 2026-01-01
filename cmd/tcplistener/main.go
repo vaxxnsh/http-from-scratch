de9ad234f48d940ce18e7b0db4334483a5fc110b
vaxxnsh <kakshit817@gmail.com>
2026-01-02 02:46:23 +0530

    task: http-from-scratch ch-4 l-6

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,71 +1,36 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/vaxxnsh/http-from-scratch/internal/request"
 )
 
 const SERVER_PORT = ":42069"
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out)
-
-		part := ""
-
-		for {
-			buf := make([]byte, 8)
-			n, err := f.Read(buf)
-			if err != nil {
-				break
-			}
-
-			data := buf[:n]
-
-			for {
-				idx := bytes.IndexByte(data, '\n')
-				if idx == -1 {
-					part += string(data)
-					break
-				}
-
-				part += string(bytes.TrimSuffix(data[:idx], []byte("\r")))
-				out <- part
-				part = ""
-
-				data = data[idx+1:]
-			}
-		}
-
-		if len(part) > 0 {
-			out <- part
-		}
-	}()
-
-	return out
-}
 
 func main() {
 	listener, err := net.Listen("tcp", SERVER_PORT)
 
 	if err != nil {
-		log.Fatalf("error:  %s", err)
+		log.Fatalf("error:  %s\n", err)
 	}
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Fatalf("error:  %s", err)
+			log.Fatalf("error:  %s\n", err)
 		}
 
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("'%s'\n", line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("error: %s\n", err)
 		}
+
+		fmt.Printf("Request Line: \n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 	}
 }
