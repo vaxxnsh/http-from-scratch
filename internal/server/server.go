a73d60cd91df7fdd1feebc2e407db35051d668d3
vaxxnsh <kakshit817@gmail.com>
2026-01-07 07:41:32 +0530

    task: http-from-scratch ch-7 l-1

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,29 +7,35 @@ import (
 )
 
 type Server struct {
-	port uint16
+	port   uint16
+	closed bool
 }
 
-func runConnection(s *Server, conn io.ReadCloser) {
-
+func runConnection(s *Server, conn io.ReadWriteCloser) {
+	out := []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello World!")
+	conn.Write(out)
+	conn.Close()
 }
 
 func runServer(s *Server, listener net.Listener) error {
 	for {
 		conn, err := listener.Accept()
+		if s.closed {
+			return nil
+		}
+
 		if err != nil {
 			return err
 		}
 
 		go runConnection(s, conn)
 	}
-
-	return nil
 }
 
 func Serve(port uint16) (*Server, error) {
 	server := &Server{
-		port: port,
+		port:   port,
+		closed: false,
 	}
 
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", server.port))
@@ -48,5 +54,6 @@ func Serve(port uint16) (*Server, error) {
 }
 
 func (s *Server) Close() error {
+	s.closed = true
 	return nil
 }
